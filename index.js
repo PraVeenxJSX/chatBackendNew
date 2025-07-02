89485e6436fdfa072e483a3bcb72c64e62136a22
PraVeenxJSX <160173872+PraVeenxJSX@users.noreply.github.com>
2025-07-02 15:43:49 +0530

    changed url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption = {
-    origin: 'http://localhost:3000',
+    origin: 'https://chatbackendnew-0hdo.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
